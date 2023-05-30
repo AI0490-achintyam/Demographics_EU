c890562adcbb1a826539f3038425366321b898c0
Rohit Santra <rohitsantra@aggregateintelligence.in>
2023-05-30 14:56:29 +0530

    References fixtures and testcases added

---
+++ tests/_fixtures/references.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+module.exports = [{
+  _id: mongoose.Types.ObjectId("643906210ef1dbad88a04a13"),
+  tableId: "B01001",
+  line: "1bundle exec middleman build",
+  indent: 0,
+  uniqueId: "B01001_001",
+  label: "total",
+  title: "sex by age",
+  universe: "total population",
+  type: "int"
+}]
