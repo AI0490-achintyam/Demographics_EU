b0f23cf0c56570a37f768f5e9f21da0e2ede5f98
Rohit Santra <rohitsantra@aggregateintelligence.in>
2023-06-09 16:47:49 +0530

    File module modified

--- tests/_fixtures/regions_.js
+++ tests/_fixtures/regions_.js
@@ -1,6 +1,5 @@
 module.exports = function (regions) {
   // Write your custom logic and return a promise
-  // return regions.createIndexes([{ centroid: "2dsphere" }, { geometry: "2dsphere" }, { name: "text" }])
   return regions.createIndexes([
     { key: { name: "text" }, name: "text_index" },
     { key: { centroid: "2dsphere" }, name: "2dsphere_index" },
