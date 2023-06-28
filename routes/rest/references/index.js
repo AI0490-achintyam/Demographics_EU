b5a64e1772180bada5cb445f5a59b0b008e074d2
Achintya Gopal Mondal <achintyam@aggregateintelligence.in>
2023-06-28 14:28:44 +0530

    /references routes modified

--- routes/rest/references/index.js
+++ routes/rest/references/index.js
@@ -4,17 +4,17 @@ module.exports = {
 
   async categories(req, res) {
     try {
-      const result = await Reference.distinct("category")
-      return res.status(200).json({ error: false, categories: result })
+      const categories = await Reference.distinct("category").lean().exec()
+      return res.status(200).json({ error: false, categories })
     } catch (error) {
       return res.status(500).json({ error: true, message: error.message })
     }
   },
 
   async attributes(req, res) {
-    const { category, page, size } = req.query
+    const { category, page = 1, size = 10 } = req.query
     try {
-      const query = { category }
+      const query = {}
 
       if (typeof category !== "undefined") {
         query.category = category
@@ -23,9 +23,9 @@ module.exports = {
       const paginationOptions = {
         page,
         limit: size,
-        // populate: [{ path: "_census" }],
-        // select: "-centroid -geometry",
-        sort: { _id: -1 }
+        select: "-id -_id",
+        sort: { attribute: 1 },
+        lean: true
       }
       const {
         docs, totalDocs,
